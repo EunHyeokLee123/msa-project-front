8842840ba8036041b7bfa4a24de88dfd0ce3c6d5
EunHyeok <secun77@naver.com>
2025-05-13 09:23:18 +0900

    tokenContext 수정

--- src/context/TokenContext.jsx
+++ src/context/TokenContext.jsx
@@ -15,3 +15,27 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
   return <AuthContext.Provider value={token}>{children}</AuthContext.Provider>;
 };
+
+// 나중에 localStorage에 있는 값들을 받아서 전역적으로 사용하게끔
+// 할 때 사용할 코드들
+// // Context 생성
+// const AuthContext = createContext(null);
+
+// // Custom Hook
+// export const useAuth = () => useContext(AuthContext);
+
+// // Provider
+// export const AuthProvider = ({ children }) => {
+//   const [token, setToken] = useState(null);
+
+//   useEffect(() => {
+//     const storedToken = localStorage.getItem('token');
+//     setToken(storedToken);
+//   }, []);
+
+//   return (
+//     <AuthContext.Provider value={token}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
