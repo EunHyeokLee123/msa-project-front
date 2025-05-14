9a457614169acd62745f93cfb67edc4bd72cbf13
EunHyeok <secun77@naver.com>
2025-05-14 16:01:00 +0900

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import OrderListComponent from './features/order/OrderListComponent';
 import AdminOrderListComponent from './features/order/AdminOrderListComponent';
 import { Dashboard } from '@mui/icons-material';
 import DashBoardPage from './features/order/DashBoardPage';
+import MyPage from './page/MyPage';
 
 function App() {
   return (
@@ -44,6 +45,7 @@ function App() {
                 <Route path='/signup' element={<SignupPage />} />
                 {/* <Route path='/items' element={<CourseDetails />} /> */}
                 <Route path='/order/cart' element={<OrderPage />} />
+                <Route path='/mypage' element={<MyPage />} />
                 <Route
                   path='/order/my-order'
                   element={<OrderListComponent />}
