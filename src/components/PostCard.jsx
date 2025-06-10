dd268b9508f9a3af197d7e3bef32ea2e60578d55
EunHyeok <secun77@naver.com>
2025-06-10 10:18:40 +0900

    병합용 push

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import PostItem from './PostItem';
 import Comment from './Comment';
-import { useLocation, useNavigate } from 'react-router-dom'; // 추가
-import { Button, Box } from '@mui/material'; // 추가
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button, Box } from '@mui/material';
 import { useAuth } from '../context/TokenContext';
 import { API_BASE_URL, POST } from '../configs/host-config';
 
