c7bb3cbde6438597ee9407b3d170341061b40ff0
EunHyeok <secun77@naver.com>
2025-06-17 13:43:42 +0900

    백엔드 호출 url 수정

--- src/configs/host-config.js
+++ src/configs/host-config.js
@@ -21,7 +21,7 @@ else if (clientHostName === 'infolearnplaydata123456.shop') {
   backendHostName = 'https://api.infolearnplaydata123456.shop';
 }
 
-//'http://ec2-3-38-145-197.ap-northeast-2.compute.amazonaws.com:8000';
+//'http://ec2-3-38-145-197.ap-northeast-2.compute.amazonaws.com:8000asdasd';
 export const API_BASE_URL = backendHostName;
 export const USER = '/user-service/user';
 export const COURSE = '/course-service/courses';
